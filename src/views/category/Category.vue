4eb68957099b63671816ec580965c43b8c7cd99c
XiaoMingTongXue7 <2437372927@qq.com>
2020-02-06 16:45:51 +0800

    tabbar的实现， 首页的部分实现

---
+++ src/views/category/Category.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>分类</h2>
+</template>
+
+<script>
+  export default {
+    name: "Category"
+  }
+</script>
+
+<style scoped>
+  .wrapper {
+    height: 150px;
+    background-color: red;
+    overflow: hidden;
+    /*overflow-y: scroll;*/
+  }
+</style>
---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>个人信息</h2>
+</template>
+
+<script>
+  export default {
+    name: "Profile",
+    components: {
+    }
+  }
+</script>
+
+<style scoped>
+  .content {
+    height: 300px;
+    background-color: red;
+    overflow: hidden;
+  }
+</style>
